d1ef12eeea8702f028074e7f022688cd985f9fe1
Pato <pato_devilla@hotmail.com>
2020-09-09 11:48:51 -0500

    first commit

---
+++ lib/elasticsearch_model_repositories.rb
@@ -0,0 +1,6 @@
+require "elasticsearch_model_repositories/version"
+
+module ElasticsearchModelRepositories
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/elasticsearch_model_repositories/version.rb
@@ -0,0 +1,3 @@
+module ElasticsearchModelRepositories
+  VERSION = "0.1.0"
+end
